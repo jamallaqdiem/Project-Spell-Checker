155878e41ea650f30d56965f5dadfbe6357525f7
Jamal Laqdiem <jlaqdiem@gmail.com>
2026-02-04 10:56:11 +0000

    feat: Implement word cleaning and dictionary comparison logic.

--- common.mjs
+++ common.mjs
@@ -1,4 +1,20 @@
+import words from './words.json' with {type:'json'}
+
+const getDictionary = [...words]
 
 export const getWordsToCheck = (text) => {
-    return text.split(/\s+/).filter(word => word.length > 0)
+    return text.split(/[\s-]+/).map(word => {
+        return word.replace(/^[^\w]+|[^\w]+$/g, "")
+    }).filter(word => word.length > 0)
+}
+
+export const isCorrectWord = (word) => {
+    if(word[0]===word[0].toUpperCase() && (word[0]!==word[0].toLowerCase())) {
+        return true
+    }
+    return getDictionary.includes(word.toLowerCase())
+}
+
+export const addingWords = (newWord) =>{
+    getDictionary.push(newWord.toLowerCase())
 }
